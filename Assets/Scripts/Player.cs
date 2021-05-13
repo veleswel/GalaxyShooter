268de734df1455ea39b6df2408c12a47a6d11fee
Oleksii <alex.vlasenko1991@gmail.com>
2021-05-13 09:58:22 -0400

    Added the project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3.5f;
+    public Vector2 screenBounds;
+    public Vector3 point;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+
+        point = new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z);
+        screenBounds = Camera.main.ScreenToWorldPoint(point);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float right = Input.GetAxis("Horizontal");
+        float up = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(right, up, 0.0f) * speed * Time.deltaTime);
+    }
+}
